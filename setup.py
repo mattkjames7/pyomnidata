0e4aec423062e7a037752a2be8c840876a1b837f
mattkjames7 <mattkjames7@gmail.com>
2021-02-01 11:07:57 +0000

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyomnidata",
-    version="0.0.1",
+    version="1.0.0",
     author="Matthew Knight James",
     author_email="mattkjames7@gmail.com",
     description="Python tool for downloading, converting and reading OMNI solar wind data.",
